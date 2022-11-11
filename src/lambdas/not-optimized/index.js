a977a6e9e6bfbd85a931f0aace08994fd9c70dfc
Matteo Depascale <m.depascale@reply.it>
2022-11-11 09:39:52 +0100

    lambda not optimized

--- src/lambdas/not-optimized/index.js
+++ src/lambdas/not-optimized/index.js
@@ -1,7 +1,7 @@
 // const { DynamoDB } = require('aws-sdk');
 // const crypto = require('crypto');
 
-exports.handler = async (event) => {
+exports.handler = async () => {
   const { DynamoDB } = require('aws-sdk');
   const crypto = require('crypto');
 
@@ -9,7 +9,7 @@ exports.handler = async (event) => {
   const LOOP_INDEX = 10;
   const ONE_DAY_IN_MILLIS = 24 * 60 * 60 * 1000;
 
-  for (let index = 0; index < LOOP_INDEX; index++) {
+  for (let index = 0; index < LOOP_INDEX; index += 1) {
     await dynamodb.put({
       TableName: process.env.DYNAMODB_TABLE,
       Item: {
