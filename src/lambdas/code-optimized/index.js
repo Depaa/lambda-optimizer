4a34644d5eb6ede19d8f45eaa9aeb47706f63080
Matteo Depascale <m.depascale@reply.it>
2022-11-15 22:22:05 +0100

    feat(lambda): update lambdas code

--- src/lambdas/code-optimized/index.js
+++ src/lambdas/code-optimized/index.js
@@ -1,24 +1,15 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const https = require('https');
 
 // eslint-disable-next-line no-unused-vars
 const dynamodb = new DynamoDBClient({ region: 'eu-central-1' });
-const LOOP_INDEX = 35;
-const URL = 'https://www.google.com/';
+const LOOP_INDEX = 34;
 
 const fibonacci = (n) => {
   if (n < 2) return 1;
   return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
-const fetchWelcomePage = async (index) => new Promise((resolve, reject) => {
-  https.get(URL, (res) => {
-    console.debug(index);
-    resolve(res.statusCode);
-  }).on('error', (e) => {
-    reject(Error(e));
-  });
-});
+const sleep = (time) => new Promise((resolve) => { setTimeout(resolve, time); });
 
 exports.handler = async () => {
   console.time('fibonacci');
@@ -28,7 +19,7 @@ exports.handler = async () => {
   console.time('total');
   const promises = [];
   for (let index = 0; index < LOOP_INDEX; index += 1) {
-    promises.push(fetchWelcomePage(index));
+    promises.push(sleep(100));
   }
   await Promise.all(promises);
   console.timeEnd('total');
--- src/lambdas/cold-start-optimized/index.js
+++ src/lambdas/cold-start-optimized/index.js
@@ -1,22 +1,13 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const https = require('https');
 
-const URL = 'https://www.google.com/';
-const LOOP_INDEX = 25;
+const LOOP_INDEX = 34;
 
 const fibonacci = (n) => {
   if (n < 2) return 1;
   return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
-const fetchWelcomePage = async (index) => new Promise((resolve, reject) => {
-  https.get(URL, (res) => {
-    console.debug(index, 'REQUEST');
-    resolve(res.statusCode);
-  }).on('error', (e) => {
-    reject(Error(e));
-  });
-});
+const sleep = (time) => new Promise((resolve) => { setTimeout(resolve, time); });
 
 exports.handler = async () => {
   console.time('initDynamo');
@@ -31,7 +22,7 @@ exports.handler = async () => {
   console.time('total');
   for (let index = 0; index < LOOP_INDEX; index += 1) {
     // eslint-disable-next-line no-await-in-loop
-    await fetchWelcomePage(index);
+    await sleep(100);
   }
   console.timeEnd('total');
 };
